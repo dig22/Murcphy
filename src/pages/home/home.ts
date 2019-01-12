97219f66efc4b22b28bc9551e6c0a38f6a633dc8
Digvijay <digv22@gmail.com>
2019-01-12 11:08:37 +0530

    update

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,42 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Morse } from '../../helpers/morse/morse';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  InputText : String = '';
+  IntermediateText : String = '';
+  OutputText : String  = '' ;
+  ShowIntermediate : Boolean = false;
+  PassThorughChiper : any =0;
 
+  constructor(public navCtrl: NavController) {
+      Morse.test();
+  }
+
+  Inittranslate(){
+    console.log(this.InputText);
+    if(this.InputText.length !=0){
+      if(this.InputText[0] == '-' || this.InputText[0] == '.' ){
+          this.TranslateToEnglish();
+      }else{
+        this.TranslateToMorse();
+      }
+    }
+  }
+
+  TranslateToEnglish(){
+     Morse.ToEnglish(this.InputText);
+  }
+
+  TranslateToMorse(){
+    this.OutputText = Morse.ToMorse(this.InputText);
   }
 
 }
+
